a07d2653dc6f2fa3d5094706ce2220b424bcf25e
CarmenChanCKY <kychan100798@gmail.com>
2023-04-16 22:37:06 +0800

    update query

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -4,33 +4,36 @@ import { getErrorMsg } from "@/middleware/error-handler/error_handler";
 
 const postRouter = express.Router();
 
-postRouter.get("/list", async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    let pages: any = req.query.pages;
+postRouter.get(
+  "/list",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let pages: any = req.query.pages;
 
-    if (
-      pages === undefined ||
-      pages === null ||
-      pages === "" ||
-      isNaN(Number(pages))
-    ) {
-      return res.status(422).end("invalid pages");
-    }
+      if (
+        pages === undefined ||
+        pages === null ||
+        pages === "" ||
+        isNaN(Number(pages))
+      ) {
+        return res.status(404).end("invalid pages");
+      }
 
-    const limit: number = 10;
-    pages = (parseInt(pages.toString()) - 1) * limit;
+      const limit: number = 10;
+      pages = (parseInt(pages.toString()) - 1) * limit;
 
-    if (pages < 0) {
-      return res.status(422).end("pages cannot smaller than or equals 0");
-    }
+      if (pages < 0) {
+        return res.status(422).end("pages cannot smaller than or equals 0");
+      }
 
-    const query = `SELECT post.title, post.date, post.content, post.slug, categories.name AS category_name, tag.tags_name
+      const query = `SELECT post.title, post.date, post.content, post.slug, categories.category_id,
+                    categories.name AS category_name, tag.tags_data
                   FROM post
-                  LEFT JOIN (SELECT post_category.post_id, category.name
+                  LEFT JOIN (SELECT post_category.post_id, post_category.category_id, category.name
                     FROM post_category
                     JOIN category ON category.id = post_category.category_id
                         AND category.data_status = 'active') as categories on categories.post_id = post.id
-                  LEFT JOIN (SELECT post_tags.post_id, JSON_ARRAYAGG(tags.name) AS tags_name
+                  LEFT JOIN (SELECT post_tags.post_id, JSON_ARRAYAGG(JSON_OBJECT("id", tags.id, "name", tags.name)) AS tags_data
                       FROM post_tags
                       JOIN tags AS tags
                           ON tags.id = post_tags.tags_id AND tags.data_status = 'active'
@@ -40,31 +43,37 @@ postRouter.get("/list", async (req: Request, res: Response, next: NextFunction) 
                   ORDER BY post.date DESC , post.id DESC
                   LIMIT ${limit} OFFSET ${pages}`;
 
-    const [result] = await dbPool.execute(query, []);
+      const [result] = await dbPool.execute(query, []);
 
-    const totalQuery = `SELECT COUNT(post.id) AS post_total
+      const totalQuery = `SELECT COUNT(post.id) AS post_total
                         FROM post AS post
                         WHERE post.data_status = 'active'`;
 
-    const [totalResult] = await dbPool.execute(totalQuery, []);
-    const data = JSON.parse(JSON.stringify(result));
-    const total = JSON.parse(JSON.stringify(totalResult));
+      const [totalResult] = await dbPool.execute(totalQuery, []);
+      const data = JSON.parse(JSON.stringify(result));
+      const total = JSON.parse(JSON.stringify(totalResult));
 
-    if (
-      Array.isArray(result) &&
-      Array.isArray(totalResult) &&
-      result.length > 0 &&
-      totalResult.length > 0
-    ) {
-      res.send({ data, total: total[0].post_total });
-    } else {
-      // throw new Error('BROKEN')
-      return res.status(422).end("record not found");
+      if (
+        Array.isArray(data) &&
+        Array.isArray(total) &&
+        data.length > 0 &&
+        total.length > 0
+      ) {
+        const regex = new RegExp("</?[^>]+(>|$)", "gi");
+        for (let i = 0; i < data.length; i++) {
+          data[i].content = data[i].content.replaceAll(regex, "");
+        }
+
+        res.send({ data, total: total[0].post_total });
+      } else {
+        // throw new Error('BROKEN')
+        return res.status(404).end("record not found");
+      }
+    } catch (error) {
+      return res.status(500).end(getErrorMsg("500", error));
     }
-  } catch (error) {
-    return res.status(500).end(getErrorMsg("500", error));
   }
-});
+);
 
 postRouter.get("/detail", async (req: Request, res: Response) => {
   try {
@@ -74,22 +83,22 @@ postRouter.get("/detail", async (req: Request, res: Response) => {
       return res.status(422).end("invalid post slug");
     }
 
-    const query = `SELECT post.title, post.date, post.content, post.slug,
-                      categories.name AS category_name, tag.tags_name, reference.reference_array
+    const query = `SELECT post.title, post.date, post.content, post.slug, categories.category_id,
+                      categories.name AS category_name, tag.tags_data, reference.reference_array
                     FROM post
-                    LEFT JOIN (SELECT post_category.post_id, category.name
+                    LEFT JOIN (SELECT post_category.post_id, post_category.category_id, category.name
                       FROM post_category
                       JOIN category ON category.id = post_category.category_id
                         AND category.data_status = 'active'
                       WHERE post_category.data_status = 'active') AS categories ON categories.post_id = post.id
-                    LEFT JOIN (SELECT post_tags.post_id, JSON_ARRAYAGG(tags.name) AS tags_name
+                    LEFT JOIN (SELECT post_tags.post_id, JSON_ARRAYAGG(JSON_OBJECT("id", tags.id, "name", tags.name)) AS tags_data
                       FROM post_tags
                       JOIN tags ON tags.id = post_tags.tags_id
                         AND tags.data_status = 'active'
                       WHERE post_tags.data_status = 'active'
                       GROUP BY post_tags.post_id) AS tag ON tag.post_id = post.id
                     LEFT JOIN (SELECT post_reference.post_id,
-                        JSON_arrayagg(JSON_OBJECT('name', post_reference.name, 'hyperlink', post_reference.hyperlink)) AS reference_array
+                        JSON_ARRAYAGG(JSON_OBJECT('name', post_reference.name, 'hyperlink', post_reference.hyperlink)) AS reference_array
                       FROM post_reference 
                       WHERE post_reference.data_status = 'active'
                       group by post_reference.post_id) AS reference ON post.id = reference.post_id
@@ -99,10 +108,10 @@ postRouter.get("/detail", async (req: Request, res: Response) => {
 
     const data = JSON.parse(JSON.stringify(result));
 
-    if (Array.isArray(result) && result.length > 0) {
-      res.send(data);
+    if (Array.isArray(data) && data.length > 0) {
+      res.send(data[0]);
     } else {
-      return res.status(422).end("record not found");
+      return res.status(404).end("record not found");
     }
   } catch (error) {
     return res.status(500).end(getErrorMsg("500", error));
